97726009ce3c0bf315ecf15f02f2ac3a9e658741
Dayvison <dayvisonxmaster@gmail.com>
2018-05-21 17:58:31 -0300

    Tela de Login

---
+++ app/src/main/java/carros/com/br/crecheepreescola/activity/Login.java
@@ -0,0 +1,15 @@
+package carros.com.br.crecheepreescola.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import carros.com.br.crecheepreescola.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
